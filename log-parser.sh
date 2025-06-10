b39043a435e23ab8069dd401b5c5484a3f862c29
Salendarsingh Gaud <sgaud@qti.qualcomm.com>
2025-06-10 09:25:49 +0530

    log-parser : Adding script to parse logs
    Script to parse the merge logs, this will search for
    "Merge sucessful: " string and capture branch name and SHA
    in a text file.
    
    Signed-off-by: Salendarsingh Gaud <sgaud@qti.qualcomm.com>

---
+++ log-parser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Usage check
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <logfile>"
+    exit 1
+fi
+
+# Input and output file names
+INPUT_FILE=$1
+MERGED_FILE="topic_SHA1"
+CONFLICT_FILE="topic_conflict"
+
+# Create or clear the output file
+> "$MERGED_FILE"
+> "$CONFLICT_FILE"
+
+# Write header
+printf "%-20s %*s\n" "Name" 20 "SHA" >> "$MERGED_FILE"
+printf "%s\n" "--------------------------------------------------------" >> "$MERGED_FILE"
+
+while read -r line; do
+ echo "$line" | grep -E "^Merge successful : .+ : [a-fA-F0-9]{7,40}$"
+ if [ $? -eq 0 ]; then
+   branch=$(echo "$line" | cut -d':' -f2 | xargs)
+   sha=$(echo "$line" | cut -d':' -f3 | xargs)
+   printf "%-20s %*s\n" "$branch" 45 "$sha" >> "$MERGED_FILE"
+ fi
+done < "$INPUT_FILE"
